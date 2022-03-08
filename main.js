570f24a5c92a396806141a1c0d4abb69c7505e63
Tiunovalera <80970079+Tiunovalera@users.noreply.github.com>
2022-03-08 13:29:48 +0300

    modufy main.js

--- main.js
+++ main.js
@@ -11,3 +11,34 @@
     window.scrollTo(0, 0);
   };
 
+
+
+
+function getDayInfo(){
+
+const currentDate = new Date();
+const currentMonth = currentDate.getMonth();
+
+//weekDay
+const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+let weekDay = currentDate.toLocaleString('ru', { weekday: 'long' });
+
+//weekNumber
+const numberOfDay = (currentDate.getDay());
+let firstDayOfMonth = new Date(currentDate.getFullYear(), currentMonth, 1);
+let weekNumber = Math.round((currentDate.getDate(currentDate) + numberOfDay + (firstDayOfMonth.getDate(currentDate)) -2) / 7 +1);
+
+
+//month
+const months = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Aвгуста", "Сентября", "Октября", "Ноября", "Декабря"];
+let month = currentDate.getMonth();
+
+//year
+let year = currentDate.getFullYear();
+
+
+
+console.log(weekDay + " " + weekNumber +" " + "неделя" + " " + (months[month]) + " " + year + " " + "года");
+}
+
+getDayInfo();
